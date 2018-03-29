75097fa0d95520160614f115c39ead95faf9cfb8
Aleksa <bigovicaleksa@gmail.com>
2018-03-29 10:22:02 +0200

    task3

--- task3.py
+++ task3.py
@@ -16,4 +16,38 @@
 ===================================================
 """
 
-# Write your code here
+def is_even(number):
+
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+
+list_length = int(input("How many numbers should list have? "))
+
+
+
+list_of_numbers = []
+
+
+for i in range(list_length):
+    new_number = int(input("Enter integer number #" + str(i+1) + ": "))
+    list_of_numbers.append(new_number)  # Add new number to the list
+
+
+total_even = total_odd = 0
+
+for number in list_of_numbers:
+
+
+    if is_even(number):
+        total_even += 1
+    else:
+        total_odd += 1
+
+
+print("List of numbers: ", str(list_of_numbers))
+print("Total even numbers: ", total_even)
+print("Total odd numbers: ", total_odd)
