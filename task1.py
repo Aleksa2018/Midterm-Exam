5d52d8568554614b7295fb7cdf663575c5d7f5fe
Aleksa <bigovicaleksa@gmail.com>
2018-03-29 08:55:37 +0200

    task1

--- task1.py
+++ task1.py
@@ -21,7 +21,9 @@
 
 
 def main():
-    # Test your function here
+    from math import pi
+    r = float(input("Input the radius of the circle : "))
+    print("The area of the circle with radius " + str(r) + " is: " + str(pi * r ** 2))
     pass
 
 main()
